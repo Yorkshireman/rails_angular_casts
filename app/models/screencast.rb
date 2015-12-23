f4da931cac08468a54689d3621e3e155f16ca216
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-12-23 21:53:07 +0000

    add tests and model validations for Screencasts

--- app/models/screencast.rb
+++ app/models/screencast.rb
@@ -1,2 +1,9 @@
 class Screencast < ActiveRecord::Base
+  validates :title, 
+            :summary,
+            :duration,
+            :link,
+            :published_at,
+            :source,
+            :video_url, presence: true
 end
--- spec/models/screencast_spec.rb
+++ spec/models/screencast_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Screencast, type: :model do
+  it{ should validate_presence_of :title }
+  it{ should validate_presence_of :summary }
+  it{ should validate_presence_of :duration }
+  it{ should validate_presence_of :link }
+  it{ should validate_presence_of :published_at }
+  it{ should validate_presence_of :source }
+  it{ should validate_presence_of :video_url }
 end
