ed1a7136b92de9286f8e8d9c0129537131a192db
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-12-30 15:43:35 +0000

    add angular, a root view (home controller), angular controller and create some basic angulary view functionality

---
+++ app/assets/javascripts/angular/controllers/screencasts_ctrl.js.coffee
@@ -0,0 +1,3 @@
+App.controller 'ScreencastsCtrl', ['$scope', ($scope) ->
+  $scope.message = "Angular is becoming less unwieldy all the time"
+]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   scope :api do
     get "/screencasts(.:format)" => "screencasts#index"
     get "/screencasts/:id(.:format)" => "screencasts#show"
   end
+
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
